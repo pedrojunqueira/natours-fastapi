d283ee9ad0e48a70385fdece2c5b0a5581891cbd
Pedro Junqueira <pedrocj@gmail.com>
2021-12-01 06:21:23 +1030

    added ratelimit functionality

--- natours/app.py
+++ natours/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI, Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from natours.routes import heart, tour_routes, user_routes
 
-app = FastAPI()
+limiter = Limiter(key_func=get_remote_address)
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
+    application.state.limiter = limiter
+    application.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
     application.include_router(
         tour_routes.router, prefix="/api/v1/tours", tags=["tours"]
     )
--- natours/routes/heart.py
+++ natours/routes/heart.py
@@ -1,9 +1,14 @@
 import fastapi
+from fastapi import Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 router = fastapi.APIRouter()
 
+limiter = Limiter(key_func=get_remote_address)
 
 @router.get("/")
-async def heart_beat():
+@limiter.limit("5/minute")
+async def heart_beat(request: Request):
 
     return {"I ❤️ FastAPI": "🙋🏽‍♂️"}
